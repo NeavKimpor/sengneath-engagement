4712c8e65a405ae801b9d18654fbb5af4f09268f
KimporNeav <neavkimpor20@kit.edu.kh>
2025-05-11 22:33:25 +0700

    fixed homepage

--- src/components/WaxSealButton.tsx
+++ src/components/WaxSealButton.tsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 function WaxSealButton() {
     return (
-        <div className="relative w-full py-8">
+        <div className="relative w-full py-8 lg:py-0 lg:mt-6">
             {/* Wax Seal and Link */}
             <div className="relative z-10 flex items-center justify-center">
                 <Link
                     to="/invitation"
-                    className="w-36 md:w-48 lg:w-64 relative cursor-pointer transition-transform duration-300 animate-zoom"
+                    className="w-36 md:w-48 relative cursor-pointer transition-transform duration-300 animate-zoom"
                 >
                     <img src={WaxSeal} alt="Wax Seal" />
                 </Link>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,21 +15,21 @@ function Home() {
             exit={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.8 }}>
-            <div className="bg-[url('/frontbg.png')] min-h-screen w-auto bg-cover bg-fixed bg-no-repeat" style={{}}>
+            <div className="bg-[url('/frontbg.png')] bg-fixed bg-cover bg-no-repeat bg-top sm:bg-[url('/frontbgland.png')] lg:bg-cover">
                 <div className="items-center text-center py-24" >                
-                    <div className="md:mt-24">
-                        <p className="moul-regular text-nude3 text-[1.4rem] md:text-3xl lg:text-5xl">សិរីមង្គល</p>
-                        <h1 className="moul-regular text-nude3 text-[1.4rem] mt-3 md:text-3xl lg:text-5xl lg:mt-10">ពិធីពិសាស្លាដក់កន្សែង</h1>
+                    <div className="md:mt-24 lg:mt-2">
+                        <p className="moul-regular text-nude3 text-[1.4rem] md:text-3xl lg:text-4xl">សិរីមង្គល</p>
+                        <h1 className="moul-regular text-nude3 text-[1.4rem] mt-3 md:text-3xl lg:text-4xl lg:mt-10">ពិធីពិសាស្លាដក់កន្សែង</h1>
                     </div>
 
-                    <img src={Logo} className="w-48 mx-auto block mt-6 md:w-72 lg:w-1/2 lg:mt-24" alt="" />
+                    <img src={Logo} className="w-48 mx-auto block mt-6 md:w-72 lg:w-80" alt="" />
                     
-                    <p className="moulpali-medium text-nude2 text-sm mt-8 md:text-xl md:mt-16 lg:text-3xl">សូមគោរពអញ្ជើញ</p>
+                    <p className="moulpali-medium text-nude2 text-sm mt-8 md:text-xl md:mt-16 lg:text-xl lg:mt-6">សូមគោរពអញ្ជើញ</p>
                     
                     <div className="pb-10">
                         <WaxSealButton/>
                     </div>
-                    <p className="metal-regular text-nude1 text-center text-sm leading-loose md:text-xl md:leading-10 lg:text-3xl lg:leading-loose">សូមមេត្តាចុចលើអេក្រង់<br></br>ដើម្បីបើកធៀបអេឡិចត្រូនិច</p>
+                    <p className="metal-regular text-nude1 text-center text-sm leading-loose md:text-xl md:leading-10 lg:text-xl lg:leading-loose">សូមមេត្តាចុចលើអេក្រង់<br></br>ដើម្បីបើកធៀបអេឡិចត្រូនិច</p>
                 </div>
                 
             </div>

4343a9eb4cb5521f4d641c39da850910aa2b89a6
KimporNeav <neavkimpor20@kit.edu.kh>
2025-05-11 22:39:35 +0700

    fixed front responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,21 +15,21 @@ function Home() {
             exit={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.8 }}>
-            <div className="bg-[url('/frontbg.png')] bg-fixed bg-cover bg-no-repeat bg-top sm:bg-[url('/frontbgland.png')] lg:bg-cover">
-                <div className="items-center text-center py-24" >                
-                    <div className="md:mt-24 lg:mt-2">
+            <div className="bg-[url('/frontbg.png')] bg-fixed bg-cover bg-no-repeat bg-center lg:bg-[url('/frontbgland.png')] lg:bg-cover">
+                <div className="items-center text-center py-9" >                
+                    <div className="lg:mt-2">
                         <p className="moul-regular text-nude3 text-[1.4rem] md:text-3xl lg:text-4xl">សិរីមង្គល</p>
                         <h1 className="moul-regular text-nude3 text-[1.4rem] mt-3 md:text-3xl lg:text-4xl lg:mt-10">ពិធីពិសាស្លាដក់កន្សែង</h1>
                     </div>
 
-                    <img src={Logo} className="w-48 mx-auto block mt-6 md:w-72 lg:w-80" alt="" />
+                    <img src={Logo} className="w-48 mx-auto block mt-6 md:w-56 lg:w-80" alt="" />
                     
-                    <p className="moulpali-medium text-nude2 text-sm mt-8 md:text-xl md:mt-16 lg:text-xl lg:mt-6">សូមគោរពអញ្ជើញ</p>
+                    <p className="moulpali-medium text-nude2 text-sm mt-8 md:text-xl md:mt-6 lg:text-xl lg:mt-6">សូមគោរពអញ្ជើញ</p>
                     
-                    <div className="pb-10">
+                    <div className="pb-3">
                         <WaxSealButton/>
                     </div>
-                    <p className="metal-regular text-nude1 text-center text-sm leading-loose md:text-xl md:leading-10 lg:text-xl lg:leading-loose">សូមមេត្តាចុចលើអេក្រង់<br></br>ដើម្បីបើកធៀបអេឡិចត្រូនិច</p>
+                    <p className="metal-regular text-nude1 text-center text-sm leading-loose md:text-lg md:leading-10 lg:text-xl lg:leading-loose">សូមមេត្តាចុចលើអេក្រង់<br></br>ដើម្បីបើកធៀបអេឡិចត្រូនិច</p>
                 </div>
                 
             </div>

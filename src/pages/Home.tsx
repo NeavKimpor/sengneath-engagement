8175a8203dddd2a59218ab9d3a4a9aa8935c500e
KimporNeav <neavkimpor20@kit.edu.kh>
2025-05-11 15:31:26 +0700

    structure logo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import OpeningButton from "../components/OpeningButton";
 import WaxSealButton from "../components/WaxSealButton";
-import Logo from "/logofront.png"
+import Logo from "/logo-md.png"
 
 function Home() {
     
     return (
         <div className="min-h-screen bg-no-repeat bg-cover bg-center" style={{}}>
             <div className="home flex flex-col items-center" >
-                <img src={Logo} className="logo w-64 max-w-md h-auto mx-auto pt-16 pb-8" alt="" />
+                <img src={Logo} className="logo w-80 max-w-md h-auto mx-auto pt-16 pb-8" alt="" />
                 <p className="kantumruy-regular text-[#d3c8b3] text-md">សូមគោរពអញ្ជើញ</p>
                 <h1 className="moul-regular text-[#f4ead0] text-xl mt-3">សិរីមង្គលពិធីពិសាស្លាដក់កន្សែង</h1>
                 <div className="pt-8 pb-10">
